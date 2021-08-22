c005ec3410def75681b2479fe6be2144de654eab
doritozbae <doritosbae@mail.ru>
2021-08-23 01:00:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == null || array.length == 0) {
+    return 0;
+  }
+  else {
+  let minEl = Math.min(...array); 
+  return minEl;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == null || array.length == 0) {
+    return 0;
+  }
+  else {
+  let maxEl = Math.max(...array); 
+  return maxEl;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == null || array.length == 0) {
+    return 0;
+  }
+  else {
+  sum = 0;
+  for (let i = 0; i < array.length; i ++) {
+    sum += array[i];
+  }
+let avg = sum /  array.length; 
+return avg; 
+  }
 }
